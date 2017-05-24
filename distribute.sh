c69e70d851d05f6d71c600e35a9f18ca22458653
Xiaoning Yue <xiaoning.yue@grabtaxi.com>
2017-05-24 16:44:55 +0800

    init repo

--- distribute.sh
+++ distribute.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 CURRENT_DIR=~/mydotfile
 cp ${CURRENT_DIR}/.bash_profile ~/.bash_profile
-cp ${CURRENT_DIR}/.zshrc ~/.zshrc
-cp -r ${CURRENT_DIR}/.oh-my-zsh ~/
 
 cp ${CURRENT_DIR}/.tmux.conf ~/.tmux.conf
 
+if [ ! -d "~/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+cp ${CURRENT_DIR}/mytheme.zsh-theme ~/.oh-my-zsh/themes/mytheme.zsh-theme
+cp ${CURRENT_DIR}/.zshrc ~/.zshrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ${CURRENT_DIR}/.vimrc ~/.vimrc
---
+++ mytheme.zsh-theme
@@ -0,0 +1,2 @@
+local ret_status="%(?:%{$reset_color%}%{$fg[yellow]%}➜ :%{$fg_bold[red]%}➜ )"
+PROMPT='%{$reset_color%}%{$fg[magenta]%}%03c${ret_status} %{$reset_color%}'
